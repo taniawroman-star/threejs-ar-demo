4ee958c9884d28126c989cda8034ce4f8d402cef
Tania Wei Roman Weber <tania.w.roman@gmail.com>
2025-12-04 15:00:22 +0100

    df

---
+++ main4.js
@@ -0,0 +1,79 @@
+import * as THREE from "three";
+import { ARButton } from "https://unpkg.com/three/examples/jsm/webxr/ARButton.js";
+
+let container;
+let camera, scene, renderer;
+let controller;
+
+init();
+animate();
+
+function init() {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    20
+  );
+
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  light.position.set(0.5, 1, 0.25);
+  scene.add(light);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.xr.enabled = true;
+
+  // NEW API
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+  container.appendChild(renderer.domElement);
+
+  // AR Button
+  document.body.appendChild(ARButton.createButton(renderer));
+
+  // UPDATED geometry (CylinderBufferGeometry removed)
+  const geometry = new THREE.SphereGeometry(15, 32, 16);
+  geometry.rotateX(Math.PI / 2);
+
+  function onSelect() {
+    const material = new THREE.MeshBasicMaterial({
+      color: Math.random() * 0xffffff,
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+
+    mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+
+    scene.add(mesh);
+  }
+
+  controller = renderer.xr.getController(0);
+  controller.addEventListener("select", onSelect);
+  scene.add(controller);
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  renderer.setAnimationLoop(render);
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
